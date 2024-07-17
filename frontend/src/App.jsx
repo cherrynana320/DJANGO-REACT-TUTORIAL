eef0a3c145b3220913dbfc0daebc1535a0daedd3
cherrynana320 <cherrynana320@gmail.com>
2024-07-17 17:56:34 +0900

    commit messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
 import react from 'react';
+import { BrowserRouter, Routes, Route, Navigation } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Login';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function Logout() {
+  // 로그아웃하자마자 refreshToken과 accessToken을 지운다
+  localStorage.clear();
+  return <Navigate to="/login" />;
+}
+
+function RegisterAndLogout() {
+  localStorage.clear();
+  return <Register />;
+}
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,35 +3,34 @@ import { jwtDecode } from 'jwt-decode';
 import api from '../api';
 import { REFRESH_TOKEN, ACCESS_TOKEN } from '../constants';
 import { useState, useEffect } from 'react';
-import { ACCESS_TOKEN } from './../constants';
 
 function ProtectedRoute({ children }) {
   // 인증 상태 관리
   const [isAuthoruized, setIsAuthorized] = useState(null);
 
-  useEffect(() =>{
-    auth().catch(()=> setIsAuthorized(false))
-  },[])
+  // useEffect : 컴포넌트가 렌더링될 때 초기화 작업을 수행하고, 초기 상태를 설정
+  useEffect(() => {
+    // auth 함수가 실행되어 인증 상태를 확인하고, setIsAuthorized를 통해 isAuthorized 상태를 설정
+    auth().catch(() => setIsAuthorized(false));
+  }, []); // 의존성 배열이 비어있기 때문에 컴포넌트가 처음 렌더링될 때 한 번만 실행
 
   // 새 토큰을 가져오는 비동기 함수
   const refreshToken = async () => {
     // 토큰 갱신
     const refreshToken = localStorage.getItem(REFRESH_TOKEN);
     try {
-        const res = await api.post("/api/token/refresh/", {
-            refresh: refreshToken,
-        });
-        if(res.status === 200){
-           localStorage.setItem(ACCESS_TOKEN, res.data.access)
-           setIsAuthorized(true)
-        }else{
-            setIsAuthorized(false)
-        }
-    }catch(error){
-        console.log(error){
-            console.log(error)
-            setIsAuthorized(false)
-        }
+      const res = await api.post('/api/token/refresh/', {
+        refresh: refreshToken,
+      });
+      if (res.status === 200) {
+        localStorage.setItem(ACCESS_TOKEN, res.data.access);
+        setIsAuthorized(true);
+      } else {
+        setIsAuthorized(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsAuthorized(false);
     }
   };
 
@@ -39,6 +38,7 @@ function ProtectedRoute({ children }) {
   const auth = async () => {
     // 인증 확인
     const token = localStorage.getItem(ACCESS_TOKEN);
+    // 토큰이 없으면
     if (!token) {
       setIsAuthorized(false);
       return;
@@ -49,7 +49,7 @@ function ProtectedRoute({ children }) {
 
     // 토큰의 유효기간이 끝났다면
     if (tokenExpiration < now) {
-        // 리프레쉬 토큰 가져오기
+      // 리프레쉬 토큰 가져오기
       await refreshToken();
     } else {
       setIsAuthorized(true);
