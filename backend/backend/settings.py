31bdffb774be969c9a6b9445ab05e8d159718a4a
cherrynana320 <cherrynana320@gmail.com>
2024-07-25 16:20:31 +0900

    update db name

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.postgresql",
-        'NAME': os.getenv("DB_Name"),
+        'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         "PASSWORD" : os.getenv("DB_PASSWORD"),
         "HOST": os.getenv("DB_HOST"),
